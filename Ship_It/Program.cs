38e1aea051a3d4d79a58cbb24805023fb322071e
Keri <79213053+KeriGabriel@users.noreply.github.com>
2022-10-08 18:34:05 -0700

    Add project files.

---
+++ Ship_It/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ship_It
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
